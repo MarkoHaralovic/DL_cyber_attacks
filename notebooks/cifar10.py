4e23f57d6f7668f753efee0a63ad2d9de8820b8a
Marko Haralovic <mharalovic11@gmail.com>
2023-11-25 22:29:41 +0100

    Script fix

--- notebooks/cifar10.py
+++ notebooks/cifar10.py
@@ -22,8 +22,13 @@ def load_cifar10_batch(batch):
     
     return data, labels
 
-cifar_10_dataset_dir = "DL_cyber_attacks\datasets\CIFAR10\cifar-10-batches-py"
-output_dir = "DL_cyber_attacks\datasets\CIFAR10\cifar-10"
+cifar_10_dataset_dir = "DL_cyber_attacks/datasets/CIFAR10/cifar-10-batches-py"
+train_output_dir = "DL_cyber_attacks/datasets/CIFAR10/cifar-10/train"
+test_output_dir = "DL_cyber_attacks/datasets/CIFAR10/cifar-10/test"
+
+os.makedirs(train_output_dir, exist_ok=True)
+os.makedirs(test_output_dir, exist_ok=True)
+
 
 num_train_samples = 50000  # CIFAR-10 has 50,000 training samples
 x_train = np.empty((num_train_samples, 32, 32, 3), dtype="uint8")
@@ -36,8 +41,8 @@ for i in range(1, 6):
     end_idx = i * 10000
     x_train[start_idx:end_idx, :, :, :], y_train[start_idx:end_idx] = load_cifar10_batch(batch)
     
-np.save("DL_cyber_attacks/datasets/CIFAR10/cifar-10/train/data", x_train)
-np.save("DL_cyber_attacks/datasets/CIFAR10/cifar-10/train/labels", y_train)
+np.save(os.path.join(train_output_dir, "data.npy"), x_train)
+np.save(os.path.join(train_output_dir, "labels.npy"), y_train)
 
 file_path = os.path.join(cifar_10_dataset_dir, "test_batch")
 test_batch = unpickle(file_path)
@@ -45,8 +50,8 @@ x_test, y_test = load_cifar10_batch(test_batch)
 x_test = np.array(x_test, dtype='uint8')
 y_test = np.array(y_test, dtype='uint8')
 
-np.save("DL_cyber_attacks/datasets/CIFAR10/cifar-10/test/data", x_test)
-np.save("DL_cyber_attacks/datasets/CIFAR10/cifar-10/test/labels", y_test)
+np.save(os.path.join(test_output_dir, "data.npy"), x_test)
+np.save(os.path.join(test_output_dir, "labels.npy"), y_test)
 
 print('Total number of Images in the Dataset:', len(x_train) + len(x_test))
 print('Number of train images:', len(x_train))
