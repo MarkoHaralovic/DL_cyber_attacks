cf74270144ed67519b3b6889042b5ac131398168
DujeStolfa <duje.stolfa626@gmail.com>
2023-12-14 22:23:56 +0100

    Patch finetuning

--- defenses/fine_pruning/FineTuning.py
+++ defenses/fine_pruning/FineTuning.py
@@ -1,4 +1,4 @@
-'''
+"""
 This is the implement of fine-tuning proposed in [1].
 [1] Fine-Pruning: Defending Against Backdooring Attacks on Deep Neural Networks. RAID, 2018.
 
@@ -7,7 +7,7 @@ smaller learning rate since the final weights are expected to be relatively clos
 Fine-tuning is significantly faster than training a network from scratch.
 
 We use Efficient Net B0 for this purpose
-'''
+"""
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -19,27 +19,28 @@ from tqdm import tqdm
 import sys
 
 sys.path.append("../../models")
-from  efficient_net_functions import load_model, _train,  test, evaluate_model, save_model
+from efficient_net_functions import load_model, _train, test, evaluate_model, save_model
 
 sys.path.append("../../notebooks")
 from Data import Data
 
-class FineTuning():
+
+class FineTuning:
     """
     FineTuning process for a neural network model using PyTorch.
 
     Args:
-        train_loader (DataLoader): DataLoader for the training dataset. 
+        train_loader (DataLoader): DataLoader for the training dataset.
             This should provide batches of data in the form of (images, labels) during training.
-        
-        test_loader (DataLoader): DataLoader for the testing dataset. 
+
+        test_loader (DataLoader): DataLoader for the testing dataset.
             Similar to train_loader, it provides batches of data for evaluating the model.
-        
-        cifar_data: Object containing the CIFAR-10 dataset. 
-            This is expected to have properties or methods to access the training and testing data, 
+
+        cifar_data: Object containing the CIFAR-10 dataset.
+            This is expected to have properties or methods to access the training and testing data,
             as well as any required preprocessing or transformations.
-        
-        device (str): The computing device to use for training and evaluation. 
+
+        device (str): The computing device to use for training and evaluation.
             Typical values are 'cpu' or 'cuda' (for using NVIDIA GPU).
             Default is 'cpu'.
 
@@ -50,68 +51,71 @@ class FineTuning():
         model (torch.nn.Module): The neural network model for fine-tuning.
         device (str): Device type ('cpu' or 'cuda').
     """
-    def __init__(self, 
-                 train_loader,
-                 test_loader,
-                 cifar_data,
-                 device = 'cpu'
-                 ):
 
-        self.train_loader  = train_loader
+    def __init__(self, train_loader, test_loader, cifar_data, device="cpu"):
+        self.train_loader = train_loader
         self.test_loader = test_loader
         self.cifar_data = cifar_data
         self.device = device
-        
+
         self.train_loss, self.valid_loss = [], []
         self.train_acc, self.valid_acc = [], []
-        
 
     def build_model(self):
         self.model = load_model(
-                n_classes = self.cifar_data.num_classes,
-                model_name='efficientnet_v2_s',
-                device=self.device,
-                pretrained=True,
-                transfer_learning=True
-                )
+            n_classes=self.cifar_data.num_classes,
+            model_name="efficientnet_v2_s",
+            device=self.device,
+            pretrained=True,
+            transfer_learning=True,
+        )
         # print(self.model)
         for param in self.model.parameters():
-            param.requires_grad = False #True ???
+            param.requires_grad = False  # True ???
 
         # Replace the classifier
         num_features = self.model.head.classifier.in_features
-        self.model.head.classifier = nn.Linear(num_features, self.cifar_data.num_classes).to(self.device)
+        self.model.head.classifier = nn.Linear(
+            num_features, self.cifar_data.num_classes
+        ).to(self.device)
         # self.model.classifier[1] = nn.Linear(in_features=num_features, out_features=self.cifar_data.num_classes).to(self.device)
         self.model = self.model.to(self.device)
-        
+
         self.count_parameters()
 
     def count_parameters(self):
         total_params = sum(p.numel() for p in self.model.parameters())
         print(f"{total_params:,} total parameters.")
-        total_trainable_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
-        print(f"{total_trainable_params:,} training parameters.")  
-        
-    def fit_model(self, epochs, optimizer, train_loader, val_loader,criterion):
+        total_trainable_params = sum(
+            p.numel() for p in self.model.parameters() if p.requires_grad
+        )
+        print(f"{total_trainable_params:,} training parameters.")
+
+    def fit_model(self, epochs, optimizer, train_loader, val_loader, criterion):
         for epoch in range(epochs):
-            epoch_loss, epoch_acc = _train(self.model, epoch, optimizer, train_loader, criterion)
+            epoch_loss, epoch_acc = _train(
+                self.model, epoch, optimizer, train_loader, criterion
+            )
             self.train_loss.append(epoch_loss)
             self.train_acc.append(epoch_acc)
-            print(f"Epoch : {epoch}, Epoch loss : {epoch_loss}, Epoch accuracy : {epoch_acc}")
-            val_loss, val_accuracy = evaluate_model(self.model, val_loader, self.device)
-            self.val_loss.append(val_loss)
-            self.val_accuracy.append(val_accuracy)
-            print(f'Epoch {epoch}: Validation Accuracy: {val_accuracy}%')
-            print('-'*50)
-        save_model(epochs,self.model,optimizer,criterion)
-            
+            print(
+                f"Epoch : {epoch}, Epoch loss : {epoch_loss}, Epoch accuracy : {epoch_acc}"
+            )
+            val_loss, val_accuracy = evaluate_model(
+                self.model, val_loader, self.device, criterion
+            )
+            self.valid_loss.append(val_loss)
+            self.valid_acc.append(val_accuracy)
+            print(f"Epoch {epoch}: Validation Accuracy: {val_accuracy}%")
+            print("-" * 50)
+        save_model(epochs, self.model, optimizer, criterion)
+
     def unfreeze_model(self, unfreeze_layers=20):
         # Unfreeze the last 20 layers
         for param in list(self.model.parameters())[-unfreeze_layers:]:
             param.requires_grad = True
-    
 
-    
+
 train_images = "..\\..\\datasets\\CIFAR10\\cifar-10\\train\\data.npy"
 train_labels = "..\\..\\datasets\\CIFAR10\\cifar-10\\train\\labels.npy"
 test_images = "..\\..\\datasets\\CIFAR10\\cifar-10\\test\\data.npy"
@@ -120,33 +124,43 @@ test_labels = "..\\..\\datasets\\CIFAR10\\cifar-10\\test\\labels.npy"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-cifar_10_dataset= Data(train_images=train_images,train_labels=train_labels,
-                     test_images=test_images,test_labels=test_labels)
+cifar_10_dataset = Data(
+    train_images=train_images,
+    train_labels=train_labels,
+    test_images=test_images,
+    test_labels=test_labels,
+)
 
 
 cifar_10_dataset.normalize()
 
 
-test_data = torch.tensor(cifar_10_dataset.test_images, dtype=torch.float32).permute(0, 3, 1, 2)
+test_data = torch.tensor(cifar_10_dataset.test_images, dtype=torch.float32).permute(
+    0, 3, 1, 2
+)
 test_labels = torch.tensor(cifar_10_dataset.test_labels, dtype=torch.long)
 test_dataset = TensorDataset(test_data, test_labels)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-train_data = torch.tensor(cifar_10_dataset.train_images, dtype=torch.float32).permute(0, 3, 1, 2)
+train_data = torch.tensor(cifar_10_dataset.train_images, dtype=torch.float32).permute(
+    0, 3, 1, 2
+)
 train_labels = torch.tensor(cifar_10_dataset.train_labels, dtype=torch.long)
 train_dataset = TensorDataset(train_data, train_labels)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=False)
 
-learning_rate = 1e-2
+learning_rate = 1e-2  # treba biti puno manji LR
 criterion = nn.CrossEntropyLoss()
 
 fineTuning = FineTuning(train_loader, test_loader, cifar_10_dataset, device)
 fineTuning.build_model()
 
 optimizer = optim.Adam(fineTuning.model.head.classifier.parameters(), lr=learning_rate)
-fineTuning.fit_model(25, optimizer, train_loader, test_loader,criterion)
+fineTuning.fit_model(25, optimizer, train_loader, test_loader, criterion)
 
 fineTuning.unfreeze_model(20)
 
-optimizer_ft = optim.Adam(filter(lambda p: p.requires_grad, fineTuning.model.parameters()), lr=1e-5)
-fineTuning.fit_model(4, optimizer_ft, train_loader, test_loader)
+optimizer_ft = optim.Adam(
+    filter(lambda p: p.requires_grad, fineTuning.model.parameters()), lr=1e-5
+)
+fineTuning.fit_model(4, optimizer_ft, train_loader, test_loader, criterion)
--- models/efficient_net_functions.py
+++ models/efficient_net_functions.py
@@ -1,9 +1,11 @@
+from tqdm import tqdm
 import torch
 import datetime
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from torchvision import transforms, models
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -20,12 +22,18 @@ def test(model, test_loader, criterion=nn.CrossEntropyLoss()):
             correct += pred.eq(target.data.view_as(pred)).long().cpu().sum().item()
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    
-def evaluate_model(model, data_loader, device):
-    model.eval()  
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
+
+
+def evaluate_model(model, data_loader, device, criterion):
+    model.eval()
     total_loss = 0.0
     total_correct = 0
     total_size = 0
@@ -47,8 +55,15 @@ def evaluate_model(model, data_loader, device):
     accuracy = 100 * total_correct / total_size
     return average_loss, accuracy
 
-    
-def _train(model, epoch, optimizer, train_loader, criterion=nn.CrossEntropyLoss(), log_interval = 100):
+
+def _train(
+    model,
+    epoch,
+    optimizer,
+    train_loader,
+    criterion=nn.CrossEntropyLoss(),
+    log_interval=100,
+):
     total_loss = 0
     total_correct = 0
     total_size = 0
@@ -65,17 +80,30 @@ def _train(model, epoch, optimizer, train_loader, criterion=nn.CrossEntropyLoss(
         loss.backward()
         optimizer.step()
         if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tAverage loss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), total_loss / total_size))
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tAverage loss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    total_loss / total_size,
+                )
+            )
 
     epoch_loss = total_loss / total_size
-    epoch_acc = 100. * (total_correct / total_size)
+    epoch_acc = 100.0 * (total_correct / total_size)
     return epoch_loss, epoch_acc
 
-            
-            
-def train(model, epochs, optimizer, train_loader, val_loader, criterion=nn.CrossEntropyLoss(), device='cpu'):
+
+def train(
+    model,
+    epochs,
+    optimizer,
+    train_loader,
+    val_loader,
+    criterion=nn.CrossEntropyLoss(),
+    device="cpu",
+):
     best_val_accuracy = 0.0
     for epoch in range(epochs):
         # Training phase
@@ -93,46 +121,77 @@ def train(model, epochs, optimizer, train_loader, val_loader, criterion=nn.Cross
             optimizer.step()
 
             if batch_idx % log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tAverage loss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), total_loss / total_size))
+                print(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tAverage loss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(data),
+                        len(train_loader.dataset),
+                        100.0 * batch_idx / len(train_loader),
+                        total_loss / total_size,
+                    )
+                )
 
         # Validation phase
-        val_accuracy = evaluate_model(model, val_loader, device)
-        print(f'Epoch {epoch}: Validation Accuracy: {val_accuracy}%')
+        val_accuracy = evaluate_model(model, val_loader, device, criterion)
+        print(f"Epoch {epoch}: Validation Accuracy: {val_accuracy}%")
 
         # Checkpointing
         if val_accuracy > best_val_accuracy:
             best_val_accuracy = val_accuracy
-            print(f'New best model found at epoch {epoch}. Saving model...')
-            torch.save(model.state_dict(), f'best_model_epoch_{epoch}.pth')
+            print(f"New best model found at epoch {epoch}. Saving model...")
+            torch.save(model.state_dict(), f"best_model_epoch_{epoch}.pth")
 
-def load_model(n_classes, model_name='efficientnet_v2_s', device='cpu',pretrained=True, transfer_learning=False):
-   if pretrained :
-        model = torch.hub.load('hankyul2/EfficientNetV2-pytorch', model_name,  nclass=n_classes,skip_validation=False)
+
+def load_model(
+    n_classes,
+    model_name="efficientnet_v2_s",
+    device="cpu",
+    pretrained=True,
+    transfer_learning=False,
+):
+    if pretrained:
+        model = torch.hub.load(
+            "hankyul2/EfficientNetV2-pytorch",
+            model_name,
+            nclass=n_classes,
+            skip_validation=False,
+        )
         model.to(device)
-   elif transfer_learning:
-    #    model = models.efficientnet_b0(pretrained=pretrained)
-        model = torch.hub.load('hankyul2/EfficientNetV2-pytorch', model_name,  nclass=n_classes,skip_validation=False)
+    elif transfer_learning:
+        #    model = models.efficientnet_b0(pretrained=pretrained)
+        model = torch.hub.load(
+            "hankyul2/EfficientNetV2-pytorch",
+            model_name,
+            nclass=n_classes,
+            skip_validation=False,
+        )
         model.to(device)
-   else:
-      from tensorflow.keras.applications import EfficientNetB0
-      model = EfficientNetB0(weights='imagenet')
-   return model
+    else:
+        from tensorflow.keras.applications import EfficientNetB0
+
+        model = EfficientNetB0(weights="imagenet")
+    return model
+
 
 import datetime
 
-def save_model(epochs, model, optimizer, criterion, pretrained):
+
+def save_model(epochs, model, optimizer, criterion):
     """
     Function to save the trained model to memory.
     """
-    current_date = datetime.datetime.now().strftime("%Y%m%d")  # Format: YYYYMMDD
+    current_date = datetime.datetime.now().strftime(
+        "%Y%m%d_%H%M"
+    )  # Format: YYYYMMDD_hhmm
 
-    filename = f"../checkpoints/efficinet_net_b0_finetune_{pretrained}_{current_date}.pth"
+    filename = f"../../checkpoints/efficinet_net_b0_finetune_{current_date}.pth"
 
-    torch.save({
-        'epoch': epochs,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'loss': criterion,
-    }, filename)
+    torch.save(
+        {
+            "epoch": epochs,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "loss": criterion,
+        },
+        filename,
+    )
