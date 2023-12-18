02df5bce4b34c113ad0ff8f962727c0b90bd6325
VMoskov <vm54341@fer.hr>
2023-12-18 21:00:26 +0100

    Added PoisonedCIFAR10 and ModifyTarget classes

--- attacks/bad_nets/BadNets.py
+++ attacks/bad_nets/BadNets.py
@@ -1,10 +1,14 @@
 import torch
+from torchvision.datasets import CIFAR10
 from torchvision.transforms import functional as F
 import PIL
 from PIL import Image
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import random
+import copy
+from torchvision.transforms import Compose
 
 
 class AddTrigger:
@@ -62,8 +66,73 @@ class AddCIFAR10Trigger(AddTrigger):
         return img
 
 
+class PoisonedCIFAR10(CIFAR10):
+    def __init__(self,
+                 benign_dataset,
+                 y_target,
+                 poisoned_rate,
+                 pattern,
+                 weight,
+                 poisoned_transform_index,
+                 poisoned_target_transform_index):
+        super(PoisonedCIFAR10, self).__init__(
+            benign_dataset.root,
+            benign_dataset.train,
+            benign_dataset.transform,
+            benign_dataset.target_transform,
+            download=True)
+        total_num = len(benign_dataset)
+        poisoned_num = int(total_num * poisoned_rate)
+        assert poisoned_num >= 0, 'poisoned_num should greater than or equal to zero.'
+        tmp_list = list(range(total_num))
+        random.shuffle(tmp_list)
+        self.poisoned_set = frozenset(tmp_list[:poisoned_num])
+
+        # Add trigger to images
+        if self.transform is None:
+            self.poisoned_transform = Compose([])
+        else:
+            self.poisoned_transform = copy.deepcopy(self.transform)
+        self.poisoned_transform.transforms.insert(poisoned_transform_index, AddCIFAR10Trigger(pattern, weight))
+
+        # Modify labels
+        if self.target_transform is None:
+            self.poisoned_target_transform = Compose([])
+        else:
+            self.poisoned_target_transform = copy.deepcopy(self.target_transform)
+        self.poisoned_target_transform.transforms.insert(poisoned_target_transform_index, ModifyTarget(y_target))
+
+    def __getitem__(self, index):
+        img, target = self.data[index], int(self.targets[index])
+
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(img)
+
+        if index in self.poisoned_set:
+            img = self.poisoned_transform(img)
+            target = self.poisoned_target_transform(target)
+        else:
+            if self.transform is not None:
+                img = self.transform(img)
+
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+
+        return img, target
+
+
+class ModifyTarget:
+    def __init__(self, y_target):
+        self.y_target = y_target
+
+    def __call__(self, y_target):
+        return self.y_target
+
+
 if __name__ == "__main__":
     print(os.getcwd())
     pattern = Image.open(r"../../resources/bad_nets/trigger_image.png")
     plt.imshow(pattern, interpolation="nearest")
     plt.show()
+    train_data = np.load(r"../../datasets/CIFAR10/cifar-10/train/data.npy")
