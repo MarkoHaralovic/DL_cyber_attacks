045506dc77c851af3ae35b631f163ead53dd35f5
VMoskov <vm54341@fer.hr>
2023-12-20 00:50:27 +0100

    changed import

--- attacks/DataPoisoning.py
+++ attacks/DataPoisoning.py
@@ -6,7 +6,7 @@ import torchvision
 from torchvision import transforms
 
 from PIL import Image
-from base import Base
+from attacks.bad_nets.base import Base
 from PoisonedCIFAR10 import PoisonedCIFAR10
 
 class BlendCIFAR10Image:
